a832fb928acd2b3a97f5bb14045043e594eddf7c
Pagis <bru65pag@gmail.com>
2020-03-01 15:26:47 +0700

    clean resizing. Color cannot be updated during resizing, Windows only trigger event after resizing completion

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ extern crate sdl2;
 
 
 // use sdl2::init;
-use std::thread::sleep;
-use std::time::Duration;
+//use std::thread::sleep;
+//use std::time::Duration;
 use sdl2::pixels::Color;
-use sdl2::rect::Rect;
+//use sdl2::rect::Rect;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
@@ -21,18 +21,33 @@ fn main() {
         .build()
         .unwrap();
 
+        canvas.set_draw_color(Color::RGB(255,0,255)); // nice pink background
+        canvas.clear(); // uses the color set in set_draw_color
+        canvas.present();
+
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => break 'main,
+                sdl2::event::Event::Quit {..} => {
+                    break 'main
+                }
+                sdl2::event::Event::Window { timestamp: _, window_id: _, win_event } => {
+                    match win_event {
+                        sdl2::event::WindowEvent::SizeChanged(_x,_y) => {
+                            canvas.set_draw_color(Color::RGB(255, 255, 0));
+                            canvas.fill_rect(None).unwrap();
+                            canvas.present();
+                        }
+                        _ => {}
+                    }
+                }
                 _ => {}
             }
+
         }
 
-        canvas.set_draw_color(Color::RGB(255,0,255));
-        canvas.clear(); // uses the color set in set_draw_color
-        canvas.present();
+
         // sleep(Duration::from_millis(5000));
 
 
