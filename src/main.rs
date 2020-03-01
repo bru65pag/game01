89bc874932c246989de2911260c997df3b89366a
Pagis <bru65pag@gmail.com>
2020-03-01 21:03:17 +0700

    draw a line

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard;
+use sdl2::rect::Point;
 
 //use sdl2::rect::Rect;
 
@@ -28,6 +29,9 @@ fn main() {
         canvas.clear(); // uses the color set in set_draw_color
         canvas.present();
 
+
+
+
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -38,9 +42,10 @@ fn main() {
                 Event::Window { timestamp: _, window_id: _, win_event } => {
                     match win_event {
                         sdl2::event::WindowEvent::SizeChanged(_x,_y) => {
-                            // canvas.set_draw_color(Color::RGB(255,0,255));
+                            canvas.set_draw_color(Color::RGB(255,0,255));
+                            canvas.clear();
                             // canvas.fill_rect(Rect::new(0,0,50,50)).unwrap();
-                            //canvas.fill_rect(None).unwrap(); // rect matches canvas size
+                            // canvas.fill_rect(None).unwrap(); // rect matches canvas size
 
                         }
                         _ => {}
@@ -51,12 +56,22 @@ fn main() {
                         match keycode.unwrap() {
                             keyboard::Keycode::P => {
                                 canvas.set_draw_color(Color::RGB(255,0,255));
+                                canvas.clear();
                             }
                             keyboard::Keycode::B => {
                                 canvas.set_draw_color(Color::RGB(0,0,255));
+                                canvas.clear();
                             }
                             keyboard::Keycode::Y => {
                                 canvas.set_draw_color(Color::RGB(255,255,0));
+                                canvas.clear();
+                            }
+                            keyboard::Keycode::L => {
+                                canvas.set_draw_color(Color::RGB(0,0,0));
+                                let x = Point::new(10,10);
+                                let y = Point::new(50,50);
+                                canvas.draw_line(x,y).unwrap();
+                                // canvas.present();
                             }
                             keyboard::Keycode::Escape => {
                                 break 'main
@@ -66,7 +81,7 @@ fn main() {
                     }
                 _ => {}
             }
-            canvas.clear();
+
             canvas.present();
         }
     }
