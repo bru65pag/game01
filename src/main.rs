94a7e2237a975d4072dafd990f836db9c1b54d3d
Pagis <bru65pag@gmail.com>
2020-03-01 13:28:06 +0700

    nice pink window getting filled by a yellow rectangle

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate sdl2;
+
+
+// use sdl2::init;
+use std::thread::sleep;
+use std::time::Duration;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let window = video.window("Window Title",400,400)
+        .position(300,200)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas()
+        .present_vsync()
+        .build()
+        .unwrap();
+
+    canvas.set_draw_color(Color::RGB(255,0,255));
+    canvas.clear(); // uses the color set in set_draw_color
+    canvas.present();
+    sleep(Duration::from_millis(5000));
+
+
+    canvas.set_draw_color(Color::RGB(255, 210, 0));
+    // canvas.fill_rect(Rect::new(0,0,50,50)).unwrap();
+    canvas.fill_rect(None).unwrap(); // will completely fill the canvas
+    canvas.present();
+
+    // let rect = sdl2::rect::Rect::new(0,0,50,50);
+
+    sleep(Duration::from_millis(5000));
+        // loop {
+        //
+        // }
+
+}
