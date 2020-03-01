b31ae3d091b8d8758e23fa089f39b2b1d1f7fa43
Pagis <bru65pag@gmail.com>
2020-03-01 16:23:58 +0700

    Implemented color change when hitting keyboard color first letter (Pink,Yellow,Blue) and leaving when hitting Escape

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate sdl2;
 //use std::thread::sleep;
 //use std::time::Duration;
 use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard;
+
 //use sdl2::rect::Rect;
 
 fn main() {
@@ -29,36 +32,42 @@ fn main() {
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => {
+                Event::Quit {..} => {
                     break 'main
                 }
-                sdl2::event::Event::Window { timestamp: _, window_id: _, win_event } => {
+                Event::Window { timestamp: _, window_id: _, win_event } => {
                     match win_event {
                         sdl2::event::WindowEvent::SizeChanged(_x,_y) => {
                             // canvas.set_draw_color(Color::RGB(255,0,255));
                             // canvas.fill_rect(Rect::new(0,0,50,50)).unwrap();
                             //canvas.fill_rect(None).unwrap(); // rect matches canvas size
-                            canvas.clear();
-                            canvas.present();
+
                         }
                         _ => {}
                     }
                 }
+                Event::KeyDown { timestamp: _, window_id: _,
+                    keycode , scancode: _, keymod: _, repeat: _ } => {
+                        match keycode.unwrap() {
+                            keyboard::Keycode::P => {
+                                canvas.set_draw_color(Color::RGB(255,0,255));
+                            }
+                            keyboard::Keycode::B => {
+                                canvas.set_draw_color(Color::RGB(0,0,255));
+                            }
+                            keyboard::Keycode::Y => {
+                                canvas.set_draw_color(Color::RGB(255,255,0));
+                            }
+                            keyboard::Keycode::Escape => {
+                                break 'main
+                            }
+                            _ => {}
+                        }
+                    }
                 _ => {}
             }
-
+            canvas.clear();
+            canvas.present();
         }
-
-
-        // sleep(Duration::from_millis(5000));
-
-
-        // canvas.set_draw_color(Color::RGB(255, 210, 0));
-        // // canvas.fill_rect(Rect::new(0,0,50,50)).unwrap();
-        // canvas.fill_rect(None).unwrap(); // will completely fill the canvas
-        // canvas.present();
     }
-
-
-
 }
