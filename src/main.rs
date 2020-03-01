7273041571b5e7bb8f8248b127ca3f94fedc9133
Pagis <bru65pag@gmail.com>
2020-03-01 13:44:33 +0700

    nice pink window that can be exited with the X button

--- src/main.rs
+++ src/main.rs
@@ -21,22 +21,27 @@ fn main() {
         .build()
         .unwrap();
 
-    canvas.set_draw_color(Color::RGB(255,0,255));
-    canvas.clear(); // uses the color set in set_draw_color
-    canvas.present();
-    sleep(Duration::from_millis(5000));
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(Color::RGB(255,0,255));
+        canvas.clear(); // uses the color set in set_draw_color
+        canvas.present();
+        // sleep(Duration::from_millis(5000));
 
 
-    canvas.set_draw_color(Color::RGB(255, 210, 0));
-    // canvas.fill_rect(Rect::new(0,0,50,50)).unwrap();
-    canvas.fill_rect(None).unwrap(); // will completely fill the canvas
-    canvas.present();
+        // canvas.set_draw_color(Color::RGB(255, 210, 0));
+        // // canvas.fill_rect(Rect::new(0,0,50,50)).unwrap();
+        // canvas.fill_rect(None).unwrap(); // will completely fill the canvas
+        // canvas.present();
+    }
 
-    // let rect = sdl2::rect::Rect::new(0,0,50,50);
 
-    sleep(Duration::from_millis(5000));
-        // loop {
-        //
-        // }
 
 }
