6f433f952e68e984a96dfe54b7c1aa738a3bee8b
Bruno Pagis <bruno@mx>
2020-04-05 10:55:47 +0200

    now asking for # of fps on command line

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,19 @@ extern crate sdl2;
 mod game;
 
 use game::game::Game;
+use std::env;
+
 
 fn main() {
-    let mut game = Game::new("rSnake".to_string());
+
+    let args:Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Missing fps");
+    };
+    let fps = args[1].parse::<u64>().unwrap();
+    println!("fps = {}", fps);
+
+    let mut game = Game::new("rSnake".to_string(), fps);
     game.get_title();
     game.game_loop();
 }
